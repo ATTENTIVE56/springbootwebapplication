cddff38e047bd3969fe921a8309e24955a1fb11f
ATTENTIVE56 <attentive.amitjais@gmail.com>
2019-06-15 20:13:28 -0700

    entity deinfed

--- src/main/java/com/gfg/amit/springbootwebapplication/model/Author.java
+++ src/main/java/com/gfg/amit/springbootwebapplication/model/Author.java
@@ -1,12 +1,26 @@
 package com.gfg.amit.springbootwebapplication.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Author {
+    @Id
+    private long id;
     private String firstName;
     private String lastName;
 
     public Author(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 
     public String getFirstName() {
